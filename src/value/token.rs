42037de440dbdba2e2c72bf8ed2381bc2e2eda65
Jan Max Meyer <jmm@phorward.de>
2022-01-22 22:01:38 +0100

    Moving builtin Tokens to value/token.rs

--- src/value/token.rs
+++ src/value/token.rs
@@ -1,9 +1,12 @@
 //! Token callables represented by Value::Token
+use linkme::distributed_slice;
 
 use super::{Dict, Object, Value};
 use crate::reader::Reader;
 use crate::vm::*;
 use charclass::{charclass, CharClass};
+
+use crate::builtin::{Builtin, BUILTINS};
 
 #[derive(Debug, Clone)]
 pub enum Token {
@@ -233,3 +236,139 @@ impl From<Token> for Value {
         Value::Object(Box::new(token))
     }
 }
+
+// Hard-coded Tokens are builtins, but they are consumable.
+
+#[distributed_slice(BUILTINS)]
+static IDENTIFIER: Builtin = Builtin {
+    name: "Identifier", // Matching C-style identifiers
+    signature: "",
+    func: |context, _args| {
+        let context = context.unwrap();
+
+        if let Some(ch) = context.runtime.reader.peek() {
+            if !ch.is_alphabetic() && ch != '_' {
+                return Err(Reject::Next);
+            }
+
+            context.runtime.reader.next();
+        } else {
+            return Err(Reject::Next);
+        }
+
+        let mut count: usize = 1;
+
+        while let Some(ch) = context.runtime.reader.peek() {
+            if !ch.is_alphanumeric() && ch != '_' {
+                break;
+            }
+
+            context.runtime.reader.next();
+            count += ch.len_utf8();
+        }
+
+        if count > 0 {
+            Ok(Accept::Push(Capture::Range(
+                context.runtime.reader.capture_last(count),
+                None,
+                5,
+            )))
+        } else {
+            Err(Reject::Next)
+        }
+    },
+};
+
+#[distributed_slice(BUILTINS)]
+static INTEGER: Builtin = Builtin {
+    name: "Integer", // Matching 64-bit integers directly
+    signature: "",
+    func: |context, _args| {
+        let context = context.unwrap();
+
+        let mut neg = false;
+        let mut value: i64 = 0;
+
+        // Sign
+        if let Some(ch) = context.runtime.reader.peek() {
+            if ch == '-' || ch == '+' {
+                neg = ch == '-';
+                context.runtime.reader.next();
+            }
+        }
+
+        let start = context.runtime.reader.tell();
+
+        // Digits
+        while let Some(ch) = context.runtime.reader.peek() {
+            if ch < '0' || ch > '9' {
+                break;
+            }
+
+            value = value * 10 + ch.to_digit(10).unwrap() as i64;
+            context.runtime.reader.next();
+        }
+
+        if start.offset < context.runtime.reader.tell().offset {
+            if neg {
+                value = -value;
+            }
+
+            Ok(Accept::Push(Capture::Value(
+                Value::Integer(value).into(),
+                None,
+                5,
+            )))
+        } else {
+            context.runtime.reader.reset(start);
+            Err(Reject::Next)
+        }
+    },
+};
+
+#[distributed_slice(BUILTINS)]
+static WORD: Builtin = Builtin {
+    name: "Word", // Matching words made of letters
+    signature: "? min max",
+    func: |context, args| {
+        let context = context.unwrap();
+
+        let min = &args[0];
+        let max = &args[1];
+
+        let mut count: usize = 0;
+
+        while let Some(ch) = context.runtime.reader.peek() {
+            if !ch.is_alphabetic() {
+                break;
+            }
+
+            context.runtime.reader.next();
+            count += ch.len_utf8();
+        }
+
+        if count > 0 {
+            if let Some(min) = min {
+                if count < min.borrow().to_usize() {
+                    count = 0;
+                }
+            }
+
+            if let Some(max) = max {
+                if count > max.borrow().to_usize() {
+                    count = 0;
+                }
+            }
+        }
+
+        if count > 0 {
+            Ok(Accept::Push(Capture::Range(
+                context.runtime.reader.capture_last(count),
+                None,
+                5,
+            )))
+        } else {
+            Err(Reject::Next)
+        }
+    },
+};
