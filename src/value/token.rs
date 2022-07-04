794f4027eac20c1614c88016ecec3af66bc68641
Jan Max Meyer <jmm@phorward.de>
2022-07-04 16:27:59 +0200

    Use of whitespace flag in Int

--- src/value/token.rs
+++ src/value/token.rs
@@ -269,9 +269,9 @@ tokay_token!("Int(base=void, with_signs=true)", {
     };
 
     if let Some(int) = if with_signs.is_true() {
-        parse_int_from_iter_with_radix::<BigInt>(context.runtime.reader, base)
+        parse_int_from_iter_with_radix::<BigInt>(context.runtime.reader, base, false)
     } else {
-        parse_uint_from_iter_with_radix::<BigInt>(context.runtime.reader, base)
+        parse_uint_from_iter_with_radix::<BigInt>(context.runtime.reader, base, false)
     } {
         Ok(Accept::Push(Capture::Value(crate::value!(int), None, 5)))
     } else {
