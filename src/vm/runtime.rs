9435550888112ea867373410bb430c55a60db432
Jan Max Meyer <jmm@phorward.de>
2022-10-27 23:48:27 +0200

    A lousy first attempt to use a writer beyond stdout

--- src/vm/runtime.rs
+++ src/vm/runtime.rs
@@ -11,9 +11,10 @@ use crate::value::RefValue;
 Holds additional runtime information, like the stack or memoization table.
 */
 pub struct Runtime<'program, 'reader> {
-    pub program: &'program Program,  // program to execute
-    pub reader: &'reader mut Reader, // reader to read from
-    pub start: usize,                // absolute start offset in relation to reader
+    pub program: &'program Program,      // program to execute
+    pub reader: &'reader mut Reader,     // reader to read from
+    pub writer: Box<dyn std::io::Write>, // writer to write to
+    pub start: usize,                    // absolute start offset in relation to reader
 
     pub memo: HashMap<(usize, usize), (Offset, Result<Accept, Reject>)>, // memoization table
     pub stack: Vec<Capture>,                                             // value stack
@@ -26,6 +27,7 @@ impl<'program, 'reader> Runtime<'program, 'reader> {
         Self {
             program,
             reader,
+            writer: Box::new(std::io::stdout()),
             start: 0,
             memo: HashMap::new(),
             stack: Vec::new(),
@@ -45,10 +47,5 @@ impl<'program, 'reader> Runtime<'program, 'reader> {
 
     pub fn save_stack(mut self) -> Vec<RefValue> {
         self.stack.drain(..).map(|item| item.get_value()).collect()
-    }
-
-    pub fn dump(&self) {
-        println!("memo has {} entries", self.memo.len());
-        println!("stack has {} entries", self.stack.len());
     }
 }
