e6696e122e61dc832538caa2cf8d9634ba6d26a6
Jan Max Meyer <jmm@phorward.de>
2022-11-21 00:46:42 +0100

    Adding prelude tests

--- src/compiler/compiler.rs
+++ src/compiler/compiler.rs
@@ -603,39 +603,3 @@ fn test_unescaping() {
         Ok(Some(crate::value!(["12345€", "€12345"])))
     );
 }
-
-#[test]
-// Tests for compiler string, match and ccl escaping
-fn test_prelude() {
-    assert_eq!(
-        crate::run("Number", "123 45.67 -8 -9.10"),
-        Ok(Some(crate::value!([123, 45.67, (-8), (-9.1)])))
-    );
-
-    assert_eq!(
-        crate::run(
-            "Token",
-            "The tokay gecko reaches a total length (including tail) of 25-30 cm on average."
-        ),
-        Ok(Some(crate::value!([
-            "The",
-            "tokay",
-            "gecko",
-            "reaches",
-            "a",
-            "total",
-            "length",
-            "(",
-            "including",
-            "tail",
-            ")",
-            "of",
-            25,
-            (-30),
-            "cm",
-            "on",
-            "average",
-            "."
-        ])))
-    );
-}
